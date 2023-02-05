540909065931ba471ae2a8f9422bfd1694a4269a
Roman Guydmn <romanguydmn@gmail.com>
2023-02-05 19:23:59 +0400

    Задача 36 Семинар 5

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = new int[10];
+Console.WriteLine($"Длинна массива = {array.Length}");
+Console.WriteLine();
+int count1 = 0;
+int count2 = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+int y1 = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    y1 = array[i]%2;
+    if (y1 > 0)
+    {
+        count1 = count1 + array[i];
+    }  
+}
+double y2 = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    y2 = i%2;
+    if (y2 > 0)
+    {
+        count2 = count2 + array[i];
+    }  
+}
+foreach (int item in array)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+Console.WriteLine($" Cумма нечетных чисел = {count1}");
+Console.WriteLine($" Cумма чисел нечетных индексов = {count2}");
